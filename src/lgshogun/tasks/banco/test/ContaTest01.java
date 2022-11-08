9f0d5b4d413ffe4ecfd45ff0622a188fee1aded7
lgshogun <lauro.gpereira@gmail.com>
2022-11-08 11:31:13 -0300

    ✨ Feat: Conta com Saldo

--- src/lgshogun/tasks/banco/test/ContaTest01.java
+++ src/lgshogun/tasks/banco/test/ContaTest01.java
@@ -5,13 +5,19 @@ import lgshogun.tasks.banco.dominio.Conta;
 public class ContaTest01 {
     public static void main(String[] args) {
         Conta conta1 = new Conta();
-
         conta1.nomeTitular = "Casaubé Silva";
         conta1.numDaConta = 1272812;
         conta1.agencia = "0001";
         conta1.nomeBanco = "Banco FWV";
         conta1.saldoAtual = 0.0;
-
         conta1.imprime();
+
+        Conta conta2 = new Conta();
+        conta2.nomeTitular = "Fernando Soares";
+        conta2.numDaConta = 1275930;
+        conta2.agencia = "0001";
+        conta2.nomeBanco = "Banco FWV";
+        conta2.saldoAtual = 103320.60;
+        conta2.imprime();
     }
 }
